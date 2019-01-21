3dc0d521d80455b3ffa895345e13508238e8fe1d
denisrat <46762938+denisrat@users.noreply.github.com>
2019-01-21 18:21:39 +0200

    created module "first project"

---
+++ QA17class/src/test/java/OpenChromeBrowser.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class OpenChromeBrowser {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUP(){
+        wd= new ChromeDriver();
+    }
+
+    @Test
+    public void chromeBrowserTest(){
+        wd.get("http:/google.com");
+    }
+
+    @AfterClass
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        wd.quit();
+    }
+}
